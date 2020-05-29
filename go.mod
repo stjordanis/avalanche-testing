f075c93381b1689c34e4821812eed9df505e44d8
mieubrisse <mieubrisse@gmail.com>
2020-05-29 06:17:03 -0300

    Small dumb controller

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module kurtosis.tech/ava/test/controller
 
 go 1.13
+
+require github.com/kurtosis-tech/kurtosis v0.0.0-20200528080832-8a649d499a9e
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	// "github.com/kurtosis-tech/kurtosis/ava_commons/testsuite"
+	"fmt"
+	"os"
+)
+
 func main() {
-    println("Sup world")
+	testName := os.Args[1]
+    // testSuite := testsuite.AvaTestSuite{}
+    println(fmt.Sprintf("Would run %v", testName))
 }
