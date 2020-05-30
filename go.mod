b9fe25df70d58db10edeff4013051822264c9f62
Galen Marchetti <galenmarchetti@Galens-MacBook-Pro.local>
2020-05-30 10:46:15 -0300

    running pchain api call in test controller

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module kurtosis.tech/ava/test/controller
 
 go 1.13
+
+require (
+	github.com/imroc/req v0.3.0
+	github.com/levigross/grequests v0.0.0-20190908174114-253788527a1a
+)
--- main.go
+++ main.go
@@ -1,5 +1,64 @@
 package main
 
+import (
+    "bytes"
+    "encoding/json"
+    "io/ioutil"
+    "log"
+    "net/http"
+)
+
+// TODO TODO TODO Get this from serialization of testnet
+const (
+    TEST_TARGET_URL="http://127.0.0.1:9651/"
+    PCHAIN_ENDPOINT="ext/P"
+    RPC_BODY= `{"jsonrpc": "2.0", "method": "platform.getCurrentValidators", "params":{},"id": 1}`
+)
+
+type Validator struct {
+    StartTime string
+    EndTime string
+    StakeAmount string
+    Id string
+}
+
+type ValidatorList []*Validator
+
+type ValidatorResponse struct {
+    Jsonrpc string
+    Result map[string]ValidatorList
+    Id int
+}
+
 func main() {
     println("Sup world")
+
+    var jsonStr = []byte(RPC_BODY)
+    var jsonBuffer = bytes.NewBuffer(jsonStr)
+    log.Printf("Request as string: %s", jsonBuffer.String())
+
+    resp, err := http.Post(TEST_TARGET_URL + PCHAIN_ENDPOINT, "application/json", jsonBuffer)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    defer resp.Body.Close()
+
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    var validatorResponse ValidatorResponse
+    json.Unmarshal(body, &validatorResponse)
+
+    validatorList := validatorResponse.Result["validators"]
+
+    for _, validator := range validatorList {
+        log.Printf("Validator id: %s", validator.Id)
+    }
+
+    if len(validatorList) < 1 {
+        panic("Failed to find a single validator.")
+    }
 }
