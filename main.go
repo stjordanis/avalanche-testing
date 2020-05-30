4bf4caa5f0f0b95ea9f4d70e9a1ade8d667d5787
mieubrisse <mieubrisse@gmail.com>
2020-05-30 07:47:25 -0300

    Make test controller read a file, so we can test the network info filepath reading

--- main.go
+++ main.go
@@ -3,11 +3,19 @@ package main
 import (
 	// "github.com/kurtosis-tech/kurtosis/ava_commons/testsuite"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	testName := os.Args[1]
 	networkInfoFilepath := os.Args[2]
-    println(fmt.Sprintf("Would run %v using network data filepath %v", testName, networkInfoFilepath))
+    println(fmt.Sprintf("Would run %v:" , testName))
+
+	data, err := ioutil.ReadFile(networkInfoFilepath)
+	if err != nil {
+		// TODO make this a proper error
+		panic("Could not read file bytes!")
+	}
+	println(fmt.Sprintf("Contents of file: %v", string(data)))
 }
