723935e7b2d513b36b2e5cfad636ffd79a87112b
mieubrisse <mieubrisse@gmail.com>
2020-06-03 08:26:57 -0300

    Update CI script to use latest convenience scripts

--- .ci/run_and_validate_pchain_test.sh
+++ .ci/run_and_validate_pchain_test.sh
@@ -2,33 +2,23 @@ set -euo pipefail
 SCRIPT_DIRPATH="$(cd "$(dirname "${0}")" && pwd)"
 ROOT_DIRPATH="$(dirname "${SCRIPT_DIRPATH}")"
 
+DEFAULT_CONTROLLER_TAG="kurtosistech/ava-e2e-tests_controller"
+
+# TODO do we even need to pull here? Kurtosis should pull this image automatically
 DEFAULT_GECKO_IMAGE="kurtosistech/gecko:latest"
 docker pull "${DEFAULT_GECKO_IMAGE}"
 
-bash "${ROOT_DIRPATH}"/scripts/build_controller_image.sh
-LATEST_CONTROLLER_TAG="kurtosistech/ava-e2e-tests_controller"
-
-bash "${ROOT_DIRPATH}"/scripts/build.sh
-
-("${ROOT_DIRPATH}"/build/ava-e2e-tests -gecko-image-name="${DEFAULT_GECKO_IMAGE}"\
- -test-controller-image-name="${LATEST_CONTROLLER_TAG}") &
-
-kurtosis_pid="${!}"
-
-# Sleep while Kurtosis spins up testnet and runs controller to execute tests.
-sleep 90
-docker image ls
-docker ps -a
-kill "${kurtosis_pid}"
-
+bash "${ROOT_DIRPATH}/scripts/rebuild_and_run.sh"
 ACTUAL_EXIT_STATUS="$(docker ps -a --latest --filter ancestor="${LATEST_CONTROLLER_TAG}" --format="{{.Status}}")"
 EXPECTED_EXIT_STATUS="Exited \(0\).*"
 
 echo "Exit status: ${ACTUAL_EXIT_STATUS}"
 
 # Clear containers.
-echo "Clearing kurtosis testnet containers."
+echo "Clearing Ava Docker containers..."
 docker rm $(docker stop $(docker ps -a -q --filter ancestor="${DEFAULT_GECKO_IMAGE}" --format="{{.ID}}")) >/dev/null
+docker rm $(docker stop $(docker ps -a -q --filter ancestor="${LATEST_CONTROLLER_TAG}" --format="{{.ID}}")) >/dev/null
+echo "Ava Docker containers cleared successfully"
 
 if [[ ${ACTUAL_EXIT_STATUS} =~ ${EXPECTED_EXIT_STATUS} ]]
 then
