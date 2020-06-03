865f97a1bc0dc1c2d4c09e0e31848c14bd31c29c
mieubrisse <mieubrisse@gmail.com>
2020-06-03 08:16:16 -0300

    Make Gecko image configurable in build/run scripts

--- scripts/rebuild_and_run.sh
+++ scripts/rebuild_and_run.sh
@@ -3,4 +3,10 @@ script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
 
 bash "${script_dirpath}/build_controller_image.sh"
 bash "${script_dirpath}/build.sh"
-bash "${script_dirpath}/run.sh"
+
+# Allow user to override default Gecko image name if desired
+if [ "${#}" -gt 0 ]; then
+    bash "${script_dirpath}/run.sh" "${1}"
+else
+    bash "${script_dirpath}/run.sh"
+fi
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,14 @@
 set -euo pipefail
 script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
 
-GECKO_IMAGE="kurtosistech/gecko"
+GECKO_IMAGE_DEFAULT="kurtosistech/gecko"
 CONTROLLER_IMAGE="kurtosistech/ava-e2e-tests_controller:latest"
 root_dirpath="$(dirname "${script_dirpath}")"
 
-"${root_dirpath}/build/ava-e2e-tests" "--gecko-image-name=${GECKO_IMAGE}" "--test-controller-image-name=${CONTROLLER_IMAGE}"
+# Allow user to override default Gecko image name if desired
+gecko_image="${GECKO_IMAGE_DEFAULT}"
+if [ "${#}" -gt 0 ]; then
+    gecko_image="${1}"
+fi
+
+"${root_dirpath}/build/ava-e2e-tests" "--gecko-image-name=${gecko_image}" "--test-controller-image-name=${CONTROLLER_IMAGE}"
