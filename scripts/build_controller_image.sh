fd8f11a1eeaba8988703e4e46e8a17bb8598877a
mieubrisse <mieubrisse@gmail.com>
2020-06-02 22:12:18 -0300

    Remove docker build from initializer

--- scripts/build_controller_image.sh
+++ scripts/build_controller_image.sh
@@ -7,7 +7,6 @@ SCRIPT_DIRPATH=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 DOCKER_ORG="kurtosistech"
 REPO_BASE="ava-e2e-tests"
 CONTROLLER_REPO="${REPO_BASE}_controller"
-INITIALIZER_REPO="${REPO_BASE}_initializer"
 LATEST_TAG="latest"
 
 ROOT_DIRPATH="$(dirname "${SCRIPT_DIRPATH}")"
@@ -18,6 +17,4 @@ DOCKER="${DOCKER:-docker}"
 COMMIT="$(git --git-dir="${ROOT_DIRPATH}/.git" rev-parse --short HEAD)"
 
 LATEST_CONTROLLER_TAG="${DOCKER_ORG}/${CONTROLLER_REPO}:${LATEST_TAG}"
-LATEST_INITIALIZER_TAG="${DOCKER_ORG}/${INITIALIZER_REPO}:${LATEST_TAG}"
 "${DOCKER}" build -t "${LATEST_CONTROLLER_TAG}" "${ROOT_DIRPATH}" -f "${ROOT_DIRPATH}/controller/Dockerfile"
-"${DOCKER}" build -t "${LATEST_INITIALIZER_TAG}" "${ROOT_DIRPATH}" -f "${ROOT_DIRPATH}/initializer/Dockerfile"
