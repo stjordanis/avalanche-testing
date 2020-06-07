942f0434fbc72205fee0227f5efda78072bb3a1a
galenmarchetti <galenmarchetti@gmail.com>
2020-06-07 11:50:29 -0300

    putting back full test suite

--- commons/ava_testsuite/ava_test_suite.go
+++ commons/ava_testsuite/ava_test_suite.go
@@ -10,10 +10,10 @@ func (a AvaTestSuite) GetTests() map[string]testsuite.Test {
 	result := make(map[string]testsuite.Test)
 
 	// TODO these should be parameterized
-	//result["tenNodeBasicTest"] = TenNodeGeckoNetworkBasicTest{}
-	//result["tenNodeGetValidatorsTest"] = TenNodeNetworkGetValidatorsTest{}
-	//result["singleNodeBasicTest"] = SingleNodeGeckoNetworkBasicTest{}
-	//result["singleNodeGetValidatorsTest"] = SingleNodeNetworkGetValidatorsTest{}
+	result["tenNodeBasicTest"] = TenNodeGeckoNetworkBasicTest{}
+	result["tenNodeGetValidatorsTest"] = TenNodeNetworkGetValidatorsTest{}
+	result["singleNodeBasicTest"] = SingleNodeGeckoNetworkBasicTest{}
+	result["singleNodeGetValidatorsTest"] = SingleNodeNetworkGetValidatorsTest{}
 	result["fiveStakingNodeGetValidatorsTest"] = FiveNodeStakingNetworkGetValidatorsTest{}
 
 	return result
