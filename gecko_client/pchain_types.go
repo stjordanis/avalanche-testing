6c96d08168c0fcdd9bdbee8bcb56e072a4ae8e66
galenmarchetti <galenmarchetti@gmail.com>
2020-06-16 06:28:54 -0300

    making dummy type rpcresponse

--- gecko_client/pchain_types.go
+++ gecko_client/pchain_types.go
@@ -215,3 +215,9 @@ type IssueTxResponse struct {
 	Result TransactionIDInfo	`json:"result"`
 	Id int	`json:"id"`
 }
+
+type RpcResponse struct {
+	JsonRpcVersion string	`json:"jsonrpc"`
+	Result interface{}	`json:"result"`
+	Id int	`json:"id"`
+}
