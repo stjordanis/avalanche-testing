09ecd0c1bebaeea9c3e8573a31833d3a49ceb207
galenmarchetti <galenmarchetti@gmail.com>
2020-06-26 08:26:40 -0300

    removing todo

--- commons/ava_networks/fixed_gecko_network/fixed_gecko_network.go
+++ commons/ava_networks/fixed_gecko_network/fixed_gecko_network.go
@@ -42,9 +42,6 @@ func NewFixedGeckoNetworkLoader(numNodes int, numBootNodes int, isStaking bool) 
 	if numBootNodes > numNodes {
 		return nil, stacktrace.NewError("Asked for %v boot nodes but svcNetwork only has %v nodes", numBootNodes, numNodes)
 	}
-	/*
-	  TODO Implement more than one bootnode for staking.
-	*/
 	if isStaking && numBootNodes > 5 {
 		return nil, stacktrace.NewError("Staking networks require five or fewer bootnodes.")
 	}
