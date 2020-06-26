29c34d2821661b0fde8e500179117a51a9436243
galenmarchetti <galenmarchetti@gmail.com>
2020-06-26 07:29:42 -0300

    bringing back in all tests

--- commons/ava_testsuite/ava_test_suite.go
+++ commons/ava_testsuite/ava_test_suite.go
@@ -9,11 +9,11 @@ type AvaTestSuite struct {}
 func (a AvaTestSuite) GetTests() map[string]testsuite.Test {
 	result := make(map[string]testsuite.Test)
 
-	// result["tenNodeBasicTest"] = TenNodeGeckoNetworkBasicTest{}
-	// result["tenNodeGetValidatorsTest"] = TenNodeNetworkGetValidatorsTest{}
-	// result["singleNodeBasicTest"] = SingleNodeGeckoNetworkBasicTest{}
-	// result["singleNodeGetValidatorsTest"] = SingleNodeNetworkGetValidatorsTest{}
-	// result["fiveStakingNodeGetValidatorsTest"] = FiveNodeStakingNetworkGetValidatorsTest{}
+	result["tenNodeBasicTest"] = TenNodeGeckoNetworkBasicTest{}
+	result["tenNodeGetValidatorsTest"] = TenNodeNetworkGetValidatorsTest{}
+	result["singleNodeBasicTest"] = SingleNodeGeckoNetworkBasicTest{}
+	result["singleNodeGetValidatorsTest"] = SingleNodeNetworkGetValidatorsTest{}
+	result["fiveStakingNodeGetValidatorsTest"] = FiveNodeStakingNetworkGetValidatorsTest{}
 	result["fiveStakingNodeFullyConnectedTest"] = FiveNodeStakingNetworkFullyConnectedTest{}
 	result["fiveStakingNodeXChainTransferTest"] = FiveNodeStakingNetworkXChainTransferTest{}
 	result["fiveStakingNodePChainImportTest"] = FiveNodeStakingNetworkPChainImportTest{}
