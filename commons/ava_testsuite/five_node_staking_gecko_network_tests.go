40c2225ad05127bc88f053fca96db856da07baa1
mieubrisse <mieubrisse@gmail.com>
2020-06-28 13:29:12 -0300

    Got dupe node ID test working

--- commons/ava_testsuite/five_node_staking_gecko_network_tests.go
+++ commons/ava_testsuite/five_node_staking_gecko_network_tests.go
@@ -244,12 +244,13 @@ func (f FiveNodeStakingNetworkDuplicateIdTest) Run(network interface{}, context 
 
 	// Add the first dupe node ID (should look normal from a network perspective
 	badServiceId1 := 1
+	logrus.Info("Adding first node with soon-to-be-duplicated node ID...")
 	checker1, err := castedNetwork.AddService(SAME_CERT_CONFIG_ID, badServiceId1)
 	if err != nil {
 		context.Fatal(stacktrace.Propagate(err, "Failed to create first dupe node ID service with ID %v", badServiceId1))
 	}
 	if err := checker1.WaitForStartup(); err != nil {
-		context.Fatal(stacktrace.Propagate(err, "An error occurred waiting for first dupe node ID service with ID to start", badServiceId1))
+		context.Fatal(stacktrace.Propagate(err, "An error occurred waiting for first dupe node ID service with ID %v to start", badServiceId1))
 	}
 	allServiceIds[badServiceId1] = true
 
@@ -265,7 +266,10 @@ func (f FiveNodeStakingNetworkDuplicateIdTest) Run(network interface{}, context 
 	}
 	allNodeIds[badServiceId1] = badServiceNodeId1
 
+	logrus.Info("Successfully added first node with soon-to-be-duplicated ID")
+
 	// Verify that the new node got accepted by everyone
+	logrus.Infof("Verifying that the new node with service ID %v was accepted by all bootstrappers...", badServiceId1)
 	for serviceId, _ := range allServiceIds {
 		acceptableNodeIds := make(map[string]bool)
 
@@ -286,9 +290,11 @@ func (f FiveNodeStakingNetworkDuplicateIdTest) Run(network interface{}, context 
 			verifyExpectedPeers(context, serviceId, allGeckoClients[serviceId], acceptableNodeIds, len(bootServiceIds), false)
 		}
 	}
+	logrus.Infof("New node with service ID %v was accepted by all bootstrappers", badServiceId1)
 
 	// Now, add a second node with the same ID
-	badServiceId2 := 1
+	badServiceId2 := 2
+	logrus.Infof("Adding second node with service ID %v which will be a duplicated node ID...", badServiceId2)
 	checker2, err := castedNetwork.AddService(SAME_CERT_CONFIG_ID, badServiceId2)
 	if err != nil {
 		context.Fatal(stacktrace.Propagate(err, "Failed to create second dupe node ID service with ID %v", badServiceId2))
@@ -309,9 +315,11 @@ func (f FiveNodeStakingNetworkDuplicateIdTest) Run(network interface{}, context 
 		context.Fatal(stacktrace.Propagate(err, "Could not get node ID from first dupe node ID service with ID %v", badServiceId2))
 	}
 	allNodeIds[badServiceId2] = badServiceNodeId2
+	logrus.Info("Second node added, causing duplicate node ID")
 
 	// At this point, it's undefined what happens with the two nodes with duplicate IDs; verify that the original nodes
 	//  in the network operate normally amongst themselves
+	logrus.Info("Connection behaviour to nodes with duplicate IDs is undefined, so verifying that the original nodes connect as expected...")
 	for serviceId, _ := range originalServiceIds {
 		acceptableNodeIds := make(map[string]bool)
 
@@ -334,17 +342,21 @@ func (f FiveNodeStakingNetworkDuplicateIdTest) Run(network interface{}, context 
 			verifyExpectedPeers(context, serviceId, allGeckoClients[serviceId], acceptableNodeIds, len(bootServiceIds), false)
 		}
 	}
+	logrus.Info("Verified that original nodes are still connected to each other")
 
 	// Now, kill the first dupe node to leave only the second (who everyone should connect with)
+	logrus.Info("Removing first node with duplicate ID...")
 	if err := castedNetwork.RemoveService(badServiceId1); err != nil {
 		context.Fatal(stacktrace.Propagate(err, "Could not remove the first service with duped node ID"))
 	}
 	delete(allServiceIds, badServiceId1)
 	delete(allGeckoClients, badServiceId1)
 	delete(allNodeIds, badServiceId1)
+	logrus.Info("Successfully removed first node with duplicate ID, leaving only the second")
 
 	// Now that the first duped node is gone, verify that the original node is still connected to just boot nodes and
 	//  the second duped-ID node is now accepted by the boot nodes
+	logrus.Info("Verifying that the network has connected to the second node with a previously-duplicated node ID...")
 	for serviceId, _ := range allServiceIds {
 		acceptableNodeIds := make(map[string]bool)
 
@@ -365,6 +377,7 @@ func (f FiveNodeStakingNetworkDuplicateIdTest) Run(network interface{}, context 
 			verifyExpectedPeers(context, serviceId, allGeckoClients[serviceId], acceptableNodeIds, len(bootServiceIds), false)
 		}
 	}
+	logrus.Info("Verified that the network has settled on the second node with previously-duplicated ID")
 }
 
 func (f FiveNodeStakingNetworkDuplicateIdTest) GetNetworkLoader() (testsuite.TestNetworkLoader, error) {
@@ -417,8 +430,14 @@ func verifyExpectedPeers(
 	}
 
 	actualNumPeers := len(peers)
+	var condition bool
+	if atLeast {
+		condition = actualNumPeers >= expectedNumPeers
+	} else {
+		condition = actualNumPeers == expectedNumPeers
+	}
 	context.AssertTrue(
-		actualNumPeers == expectedNumPeers,
+		condition,
 		stacktrace.NewError(
 			"Service ID %v actual num peers, %v, != expected num peers, %v",
 			serviceId,
