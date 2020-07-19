20d8a87aaa30280d2ad9ea966f5abce6fddbc0fd
mieubrisse <mieubrisse@gmail.com>
2020-07-19 10:46:37 -0300

    Parameterize GeckoService with ports so that it doesn't implicitly rely on constants from GeckoServiceInitializerCore

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # TBD
 * Use Kurtosis version that allows the user to configure network width
 * Use Kurtosis version where `TestSuiteRunner.RunTests` takes in a set of tests, rather than a list
+* Parameterize the `GeckoService` struct with ports so that it's not implicitly relying on constants from `GeckoServiceInitializerCore`
 
 # 0.5.0
 * Make the fully-connected-node test actually test staker registration by ensuring a second node sees the newly-registered-as-staker node
--- commons/ava_services/gecko_service.go
+++ commons/ava_services/gecko_service.go
@@ -1,17 +1,20 @@
 package ava_services
 
 import (
+	"github.com/docker/go-connections/nat"
 	"github.com/kurtosis-tech/kurtosis/commons/services"
 )
 
 type GeckoService struct {
 	ipAddr string
+	stakingPort nat.Port
+	jsonRpcPort nat.Port
 }
 
 func (g GeckoService) GetStakingSocket() services.ServiceSocket {
-	return *services.NewServiceSocket(g.ipAddr, stakingPort)
+	return *services.NewServiceSocket(g.ipAddr, g.stakingPort)
 }
 
 func (g GeckoService) GetJsonRpcSocket() services.ServiceSocket {
-	return *services.NewServiceSocket(g.ipAddr, httpPort)
+	return *services.NewServiceSocket(g.ipAddr, g.jsonRpcPort)
 }
