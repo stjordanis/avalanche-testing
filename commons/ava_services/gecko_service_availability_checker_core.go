131049b30e5cf53f7a070584fca7268b264f5467
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-07-23 14:35:19 -0400

    Remove no longer necessary sleep in IsServiceUp

--- commons/ava_services/gecko_service_availability_checker_core.go
+++ commons/ava_services/gecko_service_availability_checker_core.go
@@ -1,34 +1,34 @@
 package ava_services
 
 import (
+	"time"
+
 	"github.com/kurtosis-tech/ava-e2e-tests/gecko_client"
 	"github.com/kurtosis-tech/kurtosis/commons/services"
 	"github.com/palantir/stacktrace"
 	"github.com/sirupsen/logrus"
-	"time"
 )
 
-type GeckoServiceAvailabilityCheckerCore struct {}
+// Implements ServiceAvailabilityCheckerCore
+type GeckoServiceAvailabilityCheckerCore struct{}
+
+// Set 20 second initial delay to allow the initial health check
+// func (g GeckoServiceAvailabilityCheckerCore) InitialDelay() time.Duration { return 20 * time.Second }
+
 func (g GeckoServiceAvailabilityCheckerCore) IsServiceUp(toCheck services.Service, dependencies []services.Service) bool {
 	castedService := toCheck.(GeckoService)
 	jsonRpcSocket := castedService.GetJsonRpcSocket()
 	client := gecko_client.NewGeckoClient(jsonRpcSocket.GetIpAddr(), jsonRpcSocket.GetPort())
 	healthInfo, err := client.HealthApi().GetLiveness()
 	if err != nil {
-		logrus.Trace(stacktrace.Propagate(err, "Error occurred in getting liveness info"))
+		// TODO increase log level when InitialDelay is set to a conservative value
+		logrus.Info(stacktrace.Propagate(err, "Error occurred in getting liveness info"))
 		return false
 	}
 
-	// HACK HACK HACK we need to wait for bootstrapping to finish, and there is not API for this yet (in development)
-	// TODO TODO TODO once bootstrapping checker is available, use that instead of just waiting
-	if healthInfo.Healthy {
-		time.Sleep(15 * time.Second)
-	}
 	return healthInfo.Healthy
 }
 
 func (g GeckoServiceAvailabilityCheckerCore) GetTimeout() time.Duration {
 	return 90 * time.Second
 }
-
-
