d66d4c3734dae6869b8feeadf9a8111c8fedb93d
mieubrisse <mieubrisse@gmail.com>
2020-08-08 17:15:39 -0300

    Fix build script

--- .ci/run_and_validate_e2e_tests.sh
+++ .ci/run_and_validate_e2e_tests.sh
@@ -5,7 +5,7 @@ ROOT_DIRPATH="$(dirname "${SCRIPT_DIRPATH}")"
 # login to AWS for byzantine images
 aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" | docker login --username AWS --password-stdin 964377072876.dkr.ecr.us-east-1.amazonaws.com
 
-DEFAULT_CONTROLLER_TAG="kurtosistech/ava-e2e-tests_controller"
+DEFAULT_CONTROLLER_TAG="kurtosistech/ava-e2e-tests_controller"   # TODO This is hardcoded in the full_rebuild_and_run.sh script - this should be parameterized!!!!
 DEFAULT_GECKO_IMAGE="964377072876.dkr.ecr.us-east-1.amazonaws.com/gecko:latest"
 BYZANTINE_IMAGE="964377072876.dkr.ecr.us-east-1.amazonaws.com/gecko-byzantine:latest"
 
@@ -13,8 +13,9 @@ docker pull "${BYZANTINE_IMAGE}"
 
 E2E_TEST_COMMAND="${ROOT_DIRPATH}/scripts/full_rebuild_and_run.sh"
 BYZANTINE_IMAGE_ARG="--byzantine-image-name=${BYZANTINE_IMAGE}"
+GECKO_IMAGE_ARG="--gecko-image-name=${DEFAULT_GECKO_IMAGE}"
 return_code=0
-if ! bash "${E2E_TEST_COMMAND}" "${BYZANTINE_IMAGE_ARG}"; then
+if ! bash "${E2E_TEST_COMMAND}" "${BYZANTINE_IMAGE_ARG}" "${DEFAULT_GECKO_IMAGE}"; then
     echo "Ava E2E tests failed"
     return_code=1
 else
