5156051f9c337f9d30d381579296967cdc3160bd
mieubrisse <mieubrisse@gmail.com>
2020-08-08 17:20:22 -0300

    Add TODOs about changing Ava org

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
 set -euo pipefail
 script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
 
-GECKO_IMAGE_DEFAULT="kurtosistech/gecko"
-CONTROLLER_IMAGE="kurtosistech/ava-e2e-tests_controller:latest"
+GECKO_IMAGE_DEFAULT="kurtosistech/gecko"    # TODO change this to be a Gecko image from the Ava Docker repository
+CONTROLLER_IMAGE="kurtosistech/ava-e2e-tests_controller:latest"     # TODO Change this Docker org to be Ava labs
 root_dirpath="$(dirname "${script_dirpath}")"
 
 "${root_dirpath}/build/ava-e2e-tests" "--gecko-image-name=${GECKO_IMAGE_DEFAULT}" "--test-controller-image-name=${CONTROLLER_IMAGE}" ${*:-}
