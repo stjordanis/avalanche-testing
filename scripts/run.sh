25beed76b1d8aea369e5c5eab2e7659c24fcb77e
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-08-20 18:37:05 -0400

    Fix bug in run script

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@ script_dirpath="$(cd "$(dirname "${0}")" && pwd)"
 
 # This Docker image defines the gecko image used for developing e2e tests.
 GECKO_IMAGE_DEFAULT="avaplatform/gecko"
-CONTROLLER_IMAGE="avaplatform/avalanche-e2e-tests:24acf57-58a16bb"
+CONTROLLER_IMAGE="avaplatform/avalanche-e2e-tests_controller:latest"
 root_dirpath="$(dirname "${script_dirpath}")"
 
 "${root_dirpath}/build/avalanche-e2e-tests" "--gecko-image-name=${GECKO_IMAGE_DEFAULT}" "--test-controller-image-name=${CONTROLLER_IMAGE}" ${*:-}
