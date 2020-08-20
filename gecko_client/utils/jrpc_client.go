0aeb23fe1c37fd70229a86eca3522ac42ff71a2f
Aaron Buchwald <aaron.buchwald56@gmail.com>
2020-08-20 16:16:57 -0400

    Lower log level in rpc requester

--- gecko_client/utils/jrpc_client.go
+++ gecko_client/utils/jrpc_client.go
@@ -53,7 +53,7 @@ func (requester jsonRPCRequester) SendJSONRPCRequest(endpoint string, method str
 	}
 
 	url := fmt.Sprintf("%v/%v", requester.uri, endpoint)
-	logrus.Debugf("Sending request to %s:\n%s\n", url, requestBodyBytes)
+	logrus.Tracef("Sending request to %s:\n%s\n", url, requestBodyBytes)
 	resp, err := requester.client.Post(url, "application/json", bytes.NewBuffer(requestBodyBytes))
 	if err != nil {
 		return fmt.Errorf("problem while making JSON RPC POST request to %s: %s", url, err)
