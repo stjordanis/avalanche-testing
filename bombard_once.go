26e6810a75f1ec851c30d06b54c0a8732af90641
user18 <cevahir@avalabs.org>
2020-10-07 03:26:50 +0200

    Subject: decreased tx num
    Why:
    
    How:
    
    Tags:
    Author: Cevahir Demirkiran <cevahir@avalabs.org>
    Signed-off-by: user18 <cevahir@avalabs.org>

--- bombard_once.go
+++ bombard_once.go
@@ -116,7 +116,7 @@ func main() {
 		fmt.Printf("Test did not run due to: %s\n", err)
 		return
 	}
-	numTxs := uint64(20000)
+	numTxs := uint64(10000)
 	txFee := uint64(1000000)
 
 	executor := bombard.NewBombardExecutor(clients, numTxs, txFee, 10*time.Second, 2)
