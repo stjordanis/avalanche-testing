811dddf5c8a6b1819b6105e10a5394ae735b00e2
user18 <cevahir@avalabs.org>
2020-10-08 16:52:23 +0200

    Subject: increase thread, decrease num txs
    Why:
    
    How:
    
    Tags:
    Author: Cevahir Demirkiran <cevahir@avalabs.org>
    Signed-off-by: user18 <cevahir@avalabs.org>

--- bombard_once.go
+++ bombard_once.go
@@ -116,10 +116,10 @@ func main() {
 		fmt.Printf("Test did not run due to: %s\n", err)
 		return
 	}
-	numTxs := uint64(30000)
+	numTxs := uint64(5000)
 	txFee := uint64(1000000)
 
-	executor := bombard.NewBombardExecutor(clients, numTxs, txFee, 10*time.Second, 10)
+	executor := bombard.NewBombardExecutor(clients, numTxs, txFee, 10*time.Second, 15)
 	logrus.Info("Bombard executor created")
 	if err := executor.ExecuteTest(); err != nil {
 		fmt.Printf("Test failed: %s\n", err)
