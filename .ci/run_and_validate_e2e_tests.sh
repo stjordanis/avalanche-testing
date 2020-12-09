d1da914e50a29e7280a1aa2687b76934ece8d23e
Pedro Gomes <otherview@gmail.com>
2020-12-09 16:15:41 +0000

    updating byzantine images

--- .ci/run_and_validate_e2e_tests.sh
+++ .ci/run_and_validate_e2e_tests.sh
@@ -8,7 +8,7 @@ DOCKER_REPO="avaplatform"
 # Use stable version of Everest for CI
 AVALANCHE_IMAGE="$DOCKER_REPO/avalanchego:dev"
 # Use stable version of avalanche-byzantine based on everest for CI
-BYZANTINE_IMAGE="$DOCKER_REPO/avalanche-byzantine:v0.1.4-rc.1"
+BYZANTINE_IMAGE="$DOCKER_REPO/avalanche-byzantine:v0.1.5-rc.1"
 
 # Kurtosis will try to pull Docker images, but as of 2020-08-09 it doesn't currently support pulling from Docker repos that require authentication
 # so we have to do the pull here
--- scripts/build_and_run.sh
+++ scripts/build_and_run.sh
@@ -4,7 +4,7 @@ script_dirpath="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd)"
 # ====================== CONSTANTS =======================================================
 SUITE_IMAGE="avaplatform/avalanche-testing"
 AVALANCHE_IMAGE="avaplatform/avalanchego:dev"
-BYZANTINE_IMAGE="avaplatform/avalanche-byzantine:v0.1.4-rc.1"
+BYZANTINE_IMAGE="avaplatform/avalanche-byzantine:v0.1.5-rc.1"
 KURTOSIS_CORE_CHANNEL="1.0.3"
 INITIALIZER_IMAGE="kurtosistech/kurtosis-core_initializer:${KURTOSIS_CORE_CHANNEL}"
 API_IMAGE="kurtosistech/kurtosis-core_api:${KURTOSIS_CORE_CHANNEL}"
