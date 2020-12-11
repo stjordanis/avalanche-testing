6ab2f2306b40a27a8529eb98d532f1e4b0df0a37
Fabio Barone <holistic.computing@gmail.com>
2020-12-11 16:36:26 -0500

    first attempt at e2e matchup

--- .ci/run_and_validate_e2e_tests.sh
+++ .ci/run_and_validate_e2e_tests.sh
@@ -3,10 +3,28 @@ SCRIPT_DIRPATH="$(cd "$(dirname "${0}")" && pwd)"
 ROOT_DIRPATH="$(dirname "${SCRIPT_DIRPATH}")"
 PARALLELISM=4
 
-DOCKER_REPO="avaplatform"
+BRANCH="${TRAVIS_BRANCH}"
 
-# Use stable version of Everest for CI
-AVALANCHE_IMAGE="$DOCKER_REPO/avalanchego:v1.0.5-client"
+DOCKER_REPO="avaplatform"
+TESTING_REPO="$DOCKER_REPO/avalanche-testing"
+AVALANCHE_REPO="$DOCKER_REPO/avalanchego"
+DEFAULT_AVALANCHE_IMAGE="$DOCKER_REPO/avalanchego:dev"
+
+function docker_tag_exists() {
+    TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${DOCKER_USERNAME}'", "password": "'${DOCKER_PASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
+    curl --silent -f --head -lL https://hub.docker.com/v2/repositories/$1/tags/$2/ > /dev/null
+}
+
+if docker_tag_exists $AVALANCHE_REPO $BRANCH; then
+    echo "$AVALANCHE_REPO $BRANCH exists; using this image" 
+    AVALANCHE_IMAGE="$AVALANCHE_REPO:$BRANCH"
+else
+    echo "$AVALANCHE_REPO $BRANCH does NOT exist; using the default image" 
+    AVALANCHE_IMAGE=$DEFAULT_AVALANCHE_IMAGE
+fi
+
+echo "Using $AVALANCHE_IMAGE for CI"
+
 # Use stable version of avalanche-byzantine based on everest for CI
 BYZANTINE_IMAGE="$DOCKER_REPO/avalanche-byzantine:v0.1.4-rc.1"
 
@@ -24,6 +42,14 @@ else
 fi
 
 E2E_TEST_COMMAND="${ROOT_DIRPATH}/scripts/build_and_run.sh"
+
+#build the image
+$ROOT_DIRPATH/scripts/build_local_testsuite_image.sh
+docker tag "$AVALANCHE_TESTING_IMAGE" "$BRANCH"
+echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
+    
+# following should push all tags
+docker push $AVALANCHE_TESTING_IMAGE
 
 # Docker only allows you to have spaces in the variable if you escape them or use a Docker env file
 CUSTOM_ENV_VARS_JSON_ARG="CUSTOM_ENV_VARS_JSON={\"AVALANCHE_IMAGE\":\"${AVALANCHE_IMAGE}\",\"BYZANTINE_IMAGE\":\"${BYZANTINE_IMAGE}\"}"
