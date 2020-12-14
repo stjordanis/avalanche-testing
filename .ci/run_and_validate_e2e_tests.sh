eedd9d32709caac199210f0670e20b13f0b3cba3
Fabio Barone <holistic.computing@gmail.com>
2020-12-14 12:03:36 -0500

    remove default latest tag

--- .ci/run_and_validate_e2e_tests.sh
+++ .ci/run_and_validate_e2e_tests.sh
@@ -45,14 +45,14 @@ echo "Build the image"
 #build the image
 AVALANCHE_TESTING_IMAGE=$TESTING_REPO
 docker build -t $AVALANCHE_TESTING_IMAGE:$BRANCH . -f ${ROOT_DIRPATH}/testsuite/Dockerfile
-docker tag "$AVALANCHE_TESTING_IMAGE" "$BRANCH"
+#docker tag "$AVALANCHE_TESTING_IMAGE" "$BRANCH"
 echo "$DOCKER_PASS" | docker login --username "$DOCKER_USERNAME" --password-stdin
-
-echo "Starting build_and_run.sh"
-E2E_TEST_COMMAND="AVALANCHE_IMAGE=$AVALANCHE_IMAGE ${ROOT_DIRPATH}/scripts/build_and_run.sh"
 
 # following should push all tags
 docker push $AVALANCHE_TESTING_IMAGE
+
+echo "Starting build_and_run.sh"
+E2E_TEST_COMMAND="AVALANCHE_IMAGE=$AVALANCHE_IMAGE ${ROOT_DIRPATH}/scripts/build_and_run.sh"
 
 # Docker only allows you to have spaces in the variable if you escape them or use a Docker env file
 CUSTOM_ENV_VARS_JSON_ARG="CUSTOM_ENV_VARS_JSON={\"AVALANCHE_IMAGE\":\"${AVALANCHE_IMAGE}\",\"BYZANTINE_IMAGE\":\"${BYZANTINE_IMAGE}\"}"
