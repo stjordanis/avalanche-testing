d9b5b54293f4213320010070c09188f7031fd59a
Aaron Buchwald <aaron.buchwald56@gmail.com>
2021-01-14 15:34:38 -0500

    Improve bombard independent txs test to avoid insufficient length packer

--- testsuite/tests/bombard/bombard_independent_txs_executor.go
+++ testsuite/tests/bombard/bombard_independent_txs_executor.go
@@ -10,8 +10,6 @@ import (
 	"github.com/ava-labs/avalanche-testing/testsuite/tester"
 	"github.com/ava-labs/avalanchego/api"
 	"github.com/ava-labs/avalanchego/ids"
-	"github.com/ava-labs/avalanchego/snow/choices"
-	"github.com/ava-labs/avalanchego/utils/crypto"
 	cjson "github.com/ava-labs/avalanchego/utils/json"
 	"github.com/ava-labs/avalanchego/vms/avm"
 	"github.com/ava-labs/avalanchego/vms/components/avax"
@@ -45,21 +43,8 @@ func (e *bombardIndependentTxsExecutor) SetClients(clients []*services.Client) e
 
 // ExecuteTest implements the AvalancheTester interface
 func (e *bombardIndependentTxsExecutor) ExecuteTest() error {
-	secondaryClients := make([]*helpers.RPCWorkFlowRunner, len(e.normalClients))
-	xChainAddrs := make([]string, len(e.normalClients))
-	for i, client := range e.normalClients {
-		secondaryClients[i] = helpers.NewRPCWorkFlowRunner(
-			client,
-			api.UserPass{Username: helpers.CreateRandomString(), Password: helpers.CreateRandomString()},
-			e.acceptanceTimeout,
-		)
-		xChainAddress, _, err := secondaryClients[i].CreateDefaultAddresses()
-		if err != nil {
-			return stacktrace.Propagate(err, "Failed to create default addresses for client: %d", i)
-		}
-		xChainAddrs[i] = xChainAddress
-	}
-
+	// Create a genesis keystore user for interacting with the genesis funds
+	// without importing it to any other client
 	genesisClient := e.normalClients[0]
 	highLevelGenesisClient := helpers.NewRPCWorkFlowRunner(
 		genesisClient,
@@ -72,32 +57,35 @@ func (e *bombardIndependentTxsExecutor) ExecuteTest() error {
 	}
 	logrus.Infof("Imported genesis funds at address: %s", genesisAddress)
 
-	// Fund X Chain Addresses enough to issue [numTxs]
-	seedAmount := (e.numTxs+1)*e.txFee + e.numTxs
-	if err := highLevelGenesisClient.FundXChainAddresses(xChainAddrs, seedAmount); err != nil {
-		return stacktrace.Propagate(err, "Failed to fund X Chain Addresses for Clients")
-	}
-	logrus.Infof("Funded X Chain Addresses with seedAmount %v.", seedAmount)
+	originalUTXOAmount := e.txFee + 1
 
+	// Create secondary clients on each node and create and export a private key on each
+	// one, fund the corresponding address, and use the genesis client to fund it with sufficient
+	// UTXOs for the test.
+	secondaryClients := make([]*helpers.RPCWorkFlowRunner, len(e.normalClients))
 	codec, err := helpers.CreateXChainCodec()
 	if err != nil {
 		return stacktrace.Propagate(err, "Failed to initialize codec.")
 	}
-	for i, client := range secondaryClients {
-		// Each address should have [e.txFee] remaining after sending [numTxs] and paying the fixed fee each time
-		if err := client.VerifyXChainAVABalance(xChainAddrs[i], seedAmount); err != nil {
-			return stacktrace.Propagate(err, "Failed to verify X Chain Balane for Client: %d", i)
-		}
-	}
-	logrus.Infof("Verified X Chain Balances and retrieved UTXOs.")
 
-	// Create a string of consecutive transactions for each secondary client to send
-	privateKeys := make([]*crypto.PrivateKeySECP256K1R, len(secondaryClients))
 	txLists := make([][][]byte, len(secondaryClients))
 	txIDLists := make([][]ids.ID, len(secondaryClients))
+
 	for i, client := range e.normalClients {
-		xChainClient := client.XChainAPI()
-		pkStr, err := xChainClient.ExportKey(secondaryClients[i].User(), xChainAddrs[i])
+		secondaryClient := helpers.NewRPCWorkFlowRunner(
+			client,
+			api.UserPass{Username: helpers.CreateRandomString(), Password: helpers.CreateRandomString()},
+			e.acceptanceTimeout,
+		)
+		secondaryClients[i] = secondaryClient
+		xChainAddress, _, err := secondaryClient.CreateDefaultAddresses()
+		if err != nil {
+			return stacktrace.Propagate(err, "Failed to create default addresses for client: %d", i)
+		}
+
+		// Export formatted private key and convert it to a private key type
+		secondaryClientXChainAPI := secondaryClient.Client().XChainAPI()
+		pkStr, err := secondaryClientXChainAPI.ExportKey(secondaryClients[i].User(), xChainAddress)
 		if err != nil {
 			return stacktrace.Propagate(err, "Failed to export key.")
 		}
@@ -106,33 +94,39 @@ func (e *bombardIndependentTxsExecutor) ExecuteTest() error {
 		if err != nil {
 			return stacktrace.Propagate(err, "failed to convert private key for client %d", i)
 		}
-		privateKeys[i] = sk
 
-		// If the transactions should be independent create a transaction with [e.numTxs] outputs
-		// to create transactions that are not chained
-		sendOutputs := make([]avm.SendOutput, 0, e.numTxs)
-		originalUTXOAmount := e.txFee + 1
-		for j := 0; j < int(e.numTxs); j++ {
+		numOutputs := e.numTxs
+		if numOutputs > 2000 {
+			numOutputs = 2000
+		}
+		sendOutputs := make([]avm.SendOutput, 0, numOutputs)
+		for j := uint64(0); j < numOutputs; j++ {
 			sendOutputs = append(sendOutputs, avm.SendOutput{
 				AssetID: "AVAX",
 				Amount:  cjson.Uint64(originalUTXOAmount),
-				To:      xChainAddrs[i],
+				To:      xChainAddress,
 			})
 		}
-		txID, err := xChainClient.SendMultiple(secondaryClients[i].User(), nil, "", sendOutputs, "")
-		if err != nil {
-			return stacktrace.Propagate(err, "Failed to create UTXOs for independent transactions.")
-		}
-		status, _, err := xChainClient.ConfirmTx(txID, 5, time.Second)
-		if err != nil {
-			return err
-		}
-		if status != choices.Accepted {
-			return stacktrace.NewError("Failed to confirm create UTXOs via SendMultiple transaction %s for client %d. Status: %s.", txID, i, status)
+
+		numRemaining := e.numTxs
+		for numRemaining > 0 {
+			if len(sendOutputs) > int(numRemaining) {
+				sendOutputs = sendOutputs[:int(numRemaining)]
+			}
+			numRemaining -= uint64(len(sendOutputs))
+			txID, err := genesisClient.XChainAPI().SendMultiple(highLevelGenesisClient.User(), nil, "", sendOutputs, "")
+			if err != nil {
+				return stacktrace.Propagate(err, "Failed to send transaction with %d outputs", len(sendOutputs))
+			}
+			logrus.Infof("Sent transaction %s with %d outputs", txID, len(sendOutputs))
+
+			if err := highLevelGenesisClient.AwaitXChainTransactionAcceptance(txID); err != nil {
+				return stacktrace.Propagate(err, "Failed to confirm transaction %s", txID)
+			}
 		}
 
 		utxosBytes := make([][]byte, 0, e.numTxs)
-		utxoBytesIter, index, err := xChainClient.GetUTXOs([]string{xChainAddrs[i]}, 0, "", "")
+		utxoBytesIter, index, err := secondaryClientXChainAPI.GetUTXOs([]string{xChainAddress}, 0, "", "")
 		for {
 			if err != nil {
 				return stacktrace.Propagate(err, "Failed to get UTXOs")
@@ -141,7 +135,7 @@ func (e *bombardIndependentTxsExecutor) ExecuteTest() error {
 				break
 			}
 			utxosBytes = append(utxosBytes, utxoBytesIter...)
-			utxoBytesIter, index, err = xChainClient.GetUTXOs([]string{xChainAddrs[i]}, 0, index.Address, index.UTXO)
+			utxoBytesIter, index, err = secondaryClientXChainAPI.GetUTXOs([]string{xChainAddress}, 0, index.Address, index.UTXO)
 		}
 
 		if len(utxosBytes) != int(e.numTxs) {
